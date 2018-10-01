98c19324f0fa7046b9083f3cbab88114995da930
NathanSub <42844708+NathanSub@users.noreply.github.com>
2018-10-01 12:37:47 -0400

    Updated from Brython Server: 10/1/2018 12:37:46 PM

--- distribution.py
+++ distribution.py
@@ -36,4 +36,6 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
-words=input("Please enter a string of text (the bigger the better): ".count(' '))
+words=input("Please enter a string of text (the bigger the better): ")
+print("The distribution of characters in \"{0}\" is: ".format(words))
+print(words.count('a'))
