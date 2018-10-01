f5f19dc042c3baaa0b880eab9c14574b08e9d6ab
NathanSub <42844708+NathanSub@users.noreply.github.com>
2018-10-01 12:32:43 -0400

    Updated from Brython Server: 10/1/2018 12:32:42 PM

--- distribution.py
+++ distribution.py
@@ -36,4 +36,4 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
-input("Please enter a string of text (the bigger the better): ")
+words=input("Please enter a string of text (the bigger the better): ".count(' '))
