c2d44fa9cb6b76221e31043bab2acbcd40d40aba
NathanSub <42844708+NathanSub@users.noreply.github.com>
2018-10-02 12:53:41 -0400

    Updated from Brython Server: 10/2/2018 12:53:40 PM

--- distribution.py
+++ distribution.py
@@ -39,5 +39,9 @@ Notice about this example:
 words=input("Please enter a string of text (the bigger the better): ")
 print("The distribution of characters in \"{0}\" is: ".format(words))
 alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-nums=(int(words.count('x')))
-print(nums*'x')
+# nums=(int(words.count('x'))) #
+# print(nums*'x') #
+
+letters = words.split()
+abcletters = letters.sort()
+print(abcletters)
